889cd894b33077d80d6b1c97be8ad174f12065ab
José Sánchez-Gallego <gallegoj@uw.edu>
2017-12-07 16:13:55 -0600

    Fixing topkey

--- python/marvin/__init__.py
+++ python/marvin/__init__.py
@@ -288,7 +288,8 @@ class MarvinConfig(object):
     def _checkConfig(self):
         ''' Check the config '''
         # set and sort the base MPL dictionary
-        mpldict = {'MPL-5': ('v2_0_1', '2.0.2'),
+        mpldict = {'MPL-6': ('v2_3_1', '2.1'),
+                   'MPL-5': ('v2_0_1', '2.0.2'),
                    'MPL-4': ('v1_5_1', '1.1.1'),
                    'MPL-3': ('v1_3_3', 'v1_0_0'),
                    'MPL-2': ('v1_2_0', None),
@@ -298,7 +299,8 @@ class MarvinConfig(object):
 
         # Check the versioning config
         if not self.release:
-            topkey = list(self._mpldict)[0]
+            # topkey = list(self._mpldict)[0]
+            topkey = 'MPL-5'
             log.info('No release version set. Setting default to {0}'.format(topkey))
             self.release = topkey
 
